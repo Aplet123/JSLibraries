5c5ecf3b4435fd062ca7c165665715bb692b93e0
Aplet123 <jasonqan2004@gmail.com>
2016-02-16 20:47:55 -0500

    Minor Changes 4
    I swear they will keep haunting you.

--- Libraries/NodeNotation.js
+++ Libraries/NodeNotation.js
@@ -3,6 +3,9 @@ node.send = (function() {
     var nodes = {};
     return function(name, value, noSend) { //The parameter noSend is purely there for this library, you do not have to fill it in.
         var no = noSend;
+        if (no !== true || no !== false) {
+            no = false;
+        }
         if (!no) {
             nodes[name] = value;
             return nodes;
